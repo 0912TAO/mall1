a8491ed7a02b6e584cff4dfa58978da8ef48a274
Slowhalfframe <>
2018-10-25 17:52:57 +0800

    同步

--- mysearch/search_indexes.py
+++ mysearch/search_indexes.py
@@ -5,11 +5,8 @@ from goods.models import Goods
 class GoodsIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
-
-
     def get_model(self):
         return Goods
-
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
